4effd22e32e385a1bcfad463cb1961529c8b2a76
Marcus Mellor <marcus@mellor.com>
2019-03-19 15:56:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each (array) # put argument(s) here
   # code here
   if block_given?
     counter=0;
-    while counter<array.counter
+    while counter<array.length
       yield(array[counter])
       counter+=1
     end
