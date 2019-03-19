32303685ce6794fd1d5d7b7eabd4d957083de537
Marcus Mellor <marcus@mellor.com>
2019-03-19 15:54:42 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
 def my_each (array) # put argument(s) here
   # code here
+  if block_given?
+    counter=0;
+    while counter<array.counter
+      yield(array[counter])
+      counter+=1
+    end
+  else
+    puts "Please add a block to the method call"
+  end
 end
